a425d5684fec9fc31ff71bf9c5c9f3732b3363b0
JAMES M TACKER <jamesmtacker@James-old-work-MacBook.local>
2023-06-05 09:12:32 -0700

    added eslint check

---
+++ .eslintrc.js
@@ -0,0 +1,15 @@
+module.exports = {
+    root: true,
+    env: {
+        browser: true,
+        es2021: true,
+    },
+    extends: 'eslint:recommended',
+    parserOptions: {
+        ecmaVersion: 2021,
+        sourceType: 'module',
+    },
+    rules: {
+        // Additional rules can be added here based on your preferences
+    },
+};
